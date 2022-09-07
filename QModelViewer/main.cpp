f848f8f95fba73158d82de6c14b43756361de5a1
Charlton, Scott R <charlton@usgs.gov>
2022-09-06 19:47:06 -0600

    Cleaned up main

--- QModelViewer/main.cpp
+++ QModelViewer/main.cpp
@@ -6,28 +6,27 @@
 
 #include "mainwindow.h"
 
-/////#if defined(_MSC_VER)
-//#include <shlwapi.h>
-/////#endif
-
 int main(int argc, char** argv)
 {
-
-//#if defined(_MSC_VER)
-//    char buffer[300];
-//    PathCanonicalizeA(buffer, ".");
-//#endif
+#if defined(_DEBUG)
+  int* leak1968 = (int*)malloc(1968);
+#endif
 
   // needed to ensure appropriate OpenGL context is created for VTK rendering.
   QSurfaceFormat::setDefaultFormat(QVTKOpenGLNativeWidget::defaultFormat());
 
   QApplication app(argc, argv);
-  ////QGuiApplication::setApplicationDisplayName(QObject::tr("Model Viewer for Modflow 6"));
+  QGuiApplication::setApplicationDisplayName(QObject::tr("Model Viewer for Modflow 6"));
   QGuiApplication::setWindowIcon(QIcon(":/images/ModelViewer-16x16.png"));
 
   MainWindow window;
-  QObject::connect(&window, SIGNAL(exit), qApp, SLOT(closeAllWindows));
   window.show();
 
-  return app.exec();
+  int ret = app.exec();
+
+#if defined(_DEBUG)
+  int* leak2022 = (int*)malloc(2022);
+#endif
+
+  return ret;
 }
