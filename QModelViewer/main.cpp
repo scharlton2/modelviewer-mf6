ee57b2de9193b2fd88826cac2fe88f12febabb04
Charlton, Scott R <charlton@usgs.gov>
2022-10-01 17:28:25 -0600

    Added code to debug qt plugin loading

--- QModelViewer/main.cpp
+++ QModelViewer/main.cpp
@@ -7,6 +7,49 @@
 
 #include "mainwindow.h"
 
+#if defined(Q_OS_LINUX)
+
+#include <link.h>
+
+static int
+callback(struct dl_phdr_info *info, size_t size, void *data)
+{
+    // char *type;
+    const char *type;
+    int p_type, j;
+
+    printf("Name: \"%s\" (%d segments)\n", info->dlpi_name,
+              info->dlpi_phnum);
+
+    for (j = 0; j < info->dlpi_phnum; j++) {
+        p_type = info->dlpi_phdr[j].p_type;
+        type =  (p_type == PT_LOAD) ? "PT_LOAD" :
+                (p_type == PT_DYNAMIC) ? "PT_DYNAMIC" :
+                (p_type == PT_INTERP) ? "PT_INTERP" :
+                (p_type == PT_NOTE) ? "PT_NOTE" :
+                (p_type == PT_INTERP) ? "PT_INTERP" :
+                (p_type == PT_PHDR) ? "PT_PHDR" :
+                (p_type == PT_TLS) ? "PT_TLS" :
+                (p_type == PT_GNU_EH_FRAME) ? "PT_GNU_EH_FRAME" :
+                (p_type == PT_GNU_STACK) ? "PT_GNU_STACK" :
+                (p_type == PT_GNU_RELRO) ? "PT_GNU_RELRO" : NULL;
+
+        printf("    %2d: [%14p; memsz:%7lx] flags: 0x%x; ", j,
+                (void *) (info->dlpi_addr + info->dlpi_phdr[j].p_vaddr),
+                info->dlpi_phdr[j].p_memsz,
+                info->dlpi_phdr[j].p_flags);
+        if (type != NULL)
+            printf("%s\n", type);
+        else
+            printf("[other (0x%x)]\n", p_type);
+    }
+
+    return 0;
+}
+
+#endif // defined(Q_OS_LINUX)
+
+
 int main(int argc, char** argv)
 {
 #if defined(_DEBUG)
@@ -30,6 +73,10 @@ int main(int argc, char** argv)
 
 #if defined(_DEBUG)
   int* leak2022 = (int*)malloc(2022);
+#endif
+
+#if !defined(NDEBUG) && defined(Q_OS_LINUX)
+  dl_iterate_phdr(callback, NULL);
 #endif
 
   return ret;
